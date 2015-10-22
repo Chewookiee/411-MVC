69beff456017a42c0daf2cba40aa71881cb37132
Matthew James Puneky <matthew.puneky@gmail.com>
2015-10-22 01:15:39 -0500

    Because Location stuff works now #notAScrub

--- FoamMVC/FoamMVC/BLL/CRUD/LocationOperations/LocationCRUDBLL.cs
+++ FoamMVC/FoamMVC/BLL/CRUD/LocationOperations/LocationCRUDBLL.cs
@@ -29,13 +29,93 @@ namespace FoamMVC.BLL.CRUD.LocationOperations
         }
 
         #region Create/Update
-        public int CreateLocation(LocationViewModel viewModel)
+        public int CreateLocation(LocationCreateViewModel viewModel)
         {
             return _locationCrud.Create(new Location
             {
                 PrimaryLocation = viewModel.PrimaryLocation,
                 SecondaryLocation = viewModel.SecondaryLocation
             });
+        }
+
+        public int UpdateLocation(LocationUpdateViewModel viewModel)
+        {
+            return _locationCrud.Update(new Location
+            {
+                ID = viewModel.ID,
+                PrimaryLocation = viewModel.PrimaryLocation,
+                SecondaryLocation = viewModel.SecondaryLocation
+            });
+        }
+        #endregion
+
+        #region Get
+        public IList<LocationDisplayViewModel> GetAllLocationsForDisplay()
+        {
+            return _locationCrud.Get().Select(x => new LocationDisplayViewModel
+            {
+                ID = x.ID,
+                PrimaryLocation = x.PrimaryLocation,
+                SecondaryLocation = x.SecondaryLocation,
+                DateAdded = x.DateAdded,
+                DateUpdated = x.DateUpdated
+            }).ToList();
+        }
+
+        public LocationDisplayViewModel GetSingleLocationForDisplayByID(int id)
+        {
+            return ConvertEntityToLocationDisplayViewModel(_locationCrud.Get(id));
+        }
+
+        public LocationUpdateViewModel GetSingleLocationForUpdateByID(int id)
+        {
+            return ConvertEntityToLocationUpdateViewModel(_locationCrud.Get(id));
+        }
+
+        public LocationDeleteViewModel GetSingleLocationForDeleteByID(int id)
+        {
+            return ConvertEntityToLocationDeleteViewModel(_locationCrud.Get(id));
+        }
+        #endregion
+
+        #region Delete
+        public void DeleteLocation(LocationDeleteViewModel viewModel)
+        {
+            _locationCrud.Delete(_locationCrud.Get(viewModel.ID));
+        }
+        #endregion
+
+        #region Utils
+        private LocationDisplayViewModel ConvertEntityToLocationDisplayViewModel(Location model)
+        {
+            return new LocationDisplayViewModel
+            {
+                ID = model.ID,
+                PrimaryLocation = model.PrimaryLocation,
+                SecondaryLocation = model.SecondaryLocation,
+                DateAdded = model.DateAdded,
+                DateUpdated = model.DateUpdated
+            };
+        }
+
+        private LocationUpdateViewModel ConvertEntityToLocationUpdateViewModel(Location model)
+        {
+            return new LocationUpdateViewModel
+            {
+                ID = model.ID,
+                PrimaryLocation = model.PrimaryLocation,
+                SecondaryLocation = model.SecondaryLocation,
+            };
+        }
+
+        private LocationDeleteViewModel ConvertEntityToLocationDeleteViewModel(Location model)
+        {
+            return new LocationDeleteViewModel
+            {
+                ID = model.ID,
+                PrimaryLocation = model.PrimaryLocation,
+                SecondaryLocation = model.SecondaryLocation,
+            };
         }
         #endregion
     }
